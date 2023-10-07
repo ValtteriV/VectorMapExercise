f2ceb41ce56542a03ae141bc14900d900de5f920
ValtteriV <valtteri.vistiaho@gmail.com>
2023-10-08 02:24:39 +0300

    frontend work on login

---
+++ frontend/checkLogin.js
@@ -0,0 +1,47 @@
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user == "") {
+    const loginDialog = document.getElementById('dialog');
+    loginDialog.showModal();
+    //user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
+
+const submitLogin = document.getElementById('submit-login');
+console.log(submitLogin);
+submitLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e);
+});
+
+const submitRegister = document.getElementById('submit-register');
+submitRegister.addEventListener('click', (e) => {
+  e.preventDefault();
+});
+
+checkCookie();
